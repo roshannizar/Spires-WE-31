b64ba4d7069f618dd78ff9169eaf9c69968b62c4
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-04 14:31:28 +0530

    update

--- dist/main.65f60760.js
+++ dist/main.65f60760.js
@@ -30356,7 +30356,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55610" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "15335" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
